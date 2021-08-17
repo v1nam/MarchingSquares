b13996090cbdcceef76220d9fd49b2b59ccbc7a4
v1nam <vinamraj01@gmail.com>
2021-08-17 13:20:33 +0530

    add cellular automata and change color

--- src/main.cpp
+++ src/main.cpp
@@ -1,129 +1,83 @@
 #include "raylib.h"
-#include <cmath>
-#include <cstdlib>
 #include <ctime>
-#include <iostream>
 #include <vector>
 
-const int CELL = 30;
+const int CELL = 10;
 
 int binToInt(int a, int b, int c, int d)
 {
     return a * 1 + b * 2 + c * 4 + d * 8;
 }
 
-
-Vector2 randomGrad(int ix, int iy)
-{
-    float random =
-        2920.f * sin(ix * 21942.f + iy * 171324.f + 8912.f) * cos(ix * 23157.f * iy * 217832.f + 9758.f) * rand();
-    return Vector2{(float)cos(random), (float)sin(random)};
-}
-
-float dotProd(float x1, float y1, Vector2 grad)
-{
-    return x1 * grad.x + y1 * grad.y;
-}
-
-float interp(float a0, float a1, float w)
-{
-    return (a1 - a0) * ((w * (w * 6.0 - 15.0) + 10.0) * w * w * w) + a0;
-}
-
-float lerp(float v0, float v1, float t)
-{
-    return (1.0 - t) * v0 + v1 * t;
-}
-
-float noise2D(float x, float y)
-{
-    int x0 = (int)x;
-    int x1 = x0 + 1;
-    int y0 = (int)y;
-    int y1 = y0 + 1;
-
-    float s = dotProd(x - x0, y - y0, randomGrad(x0, y0));
-    float t = dotProd(x - x1, y - y0, randomGrad(x1, y0));
-    float u = dotProd(x - x0, y - y1, randomGrad(x0, y1));
-    float v = dotProd(x - x1, y - y1, randomGrad(x1, y1));
-
-    float sx = 3.0 * powf(x - x0, 2.0) - 2.0 * powf(x - x0, 3.0);
-    float sy = 3.0 * powf(y - y0, 2.0) - 2.0 * powf(y - y0, 3.0);
-    float a = interp(s, t, sx);
-    float b = interp(u, v, sx);
-
-    float value = interp(b, a, sy);
-    return value;
-}
-
-float perlinNoise(float x, float y, int octaves, float pers, float lum)
-{
-    float amp = 1.0;
-    float total = 0.0;
-    float freq = 1.0;
-    float max = 0.0;
-
-    for (int i{0}; i < octaves; i++)
-    {
-        freq = powf(lum, i);
-        amp = powf(pers, i);
-        total += noise2D(x * freq, y * freq) * amp;
-        max += amp;
-    }
-    return total / max;
-}
-
 void drawContour(const std::vector<std::vector<float>> &plane, int px, int py)
 {
 
-    float val1 = plane[px][py] + 1.0;
-    float val2 = plane[px + 1][py] + 1.0;
-    float val3 = plane[px + 1][py + 1] + 1.0;
-    float val4 = plane[px][py + 1] + 1.0;
+    float val1 = plane[px][py];
+    float val2 = plane[px + 1][py];
+    float val3 = plane[px + 1][py + 1];
+    float val4 = plane[px][py + 1];
 
     float cx = px * CELL;
     float cy = py * CELL;
 
-    Vector2 rlP1 = Vector2{lerp(cx, cx + CELL, (1.0 - val1) / (val2 - val1)), cy};
-    Vector2 rlP2 = Vector2{cx + CELL, lerp(cy, cy + CELL, (1.0 - val2) / (val3 - val2))};
-    Vector2 rlP3 = Vector2{cx, lerp(cy, cy + CELL, (1.0 - val1) / (val4 - val1))};
-    Vector2 rlP4 = Vector2{lerp(cx, cx + CELL, (1.0 - val4) / (val3 - val4)), cy + CELL};
+    Vector2 rlP1 = Vector2{cx + CELL/2, cy};
+    Vector2 rlP2 = Vector2{cx + CELL, cy + CELL/2};
+    Vector2 rlP3 = Vector2{cx, cy + CELL/2};
+    Vector2 rlP4 = Vector2{cx + CELL/2, cy + CELL};
 
-    switch (binToInt(ceil(val1 - 1.0), ceil(val2 - 1.0), ceil(val4 - 1.0), ceil(val3 - 1.0)))
+    switch (binToInt(val1, val2, val4, val3))
     {
     case 1:
     case 14:
-        DrawLineV(rlP1, rlP3, BLACK);
+        DrawLineV(rlP1, rlP3, RAYWHITE);
         break;
     case 2:
     case 13:
-        DrawLineV(rlP1, rlP2, BLACK);
+        DrawLineV(rlP1, rlP2, RAYWHITE);
         break;
     case 3:
     case 12:
-        DrawLineV(rlP3, rlP2, BLACK);
+        DrawLineV(rlP3, rlP2, RAYWHITE);
         break;
     case 4:
     case 11:
-        DrawLineV(rlP3, rlP4, BLACK);
+        DrawLineV(rlP3, rlP4, RAYWHITE);
         break;
     case 5:
     case 10:
-        DrawLineV(rlP1, rlP4, BLACK);
+        DrawLineV(rlP1, rlP4, RAYWHITE);
         break;
     case 6:
-        DrawLineV(rlP1, rlP3, BLACK);
-        DrawLineV(rlP2, rlP4, BLACK);
+        DrawLineV(rlP1, rlP3, RAYWHITE);
+        DrawLineV(rlP2, rlP4, RAYWHITE);
         break;
     case 7:
     case 8:
-        DrawLineV(rlP2, rlP4, BLACK);
+        DrawLineV(rlP2, rlP4, RAYWHITE);
         break;
     case 9:
-        DrawLineV(rlP1, rlP2, BLACK);
-        DrawLineV(rlP3, rlP4, BLACK);
+        DrawLineV(rlP1, rlP2, RAYWHITE);
+        DrawLineV(rlP3, rlP4, RAYWHITE);
         break;
     }
+}
+
+int getNeighbours(int x, int y, int endX, int endY, const std::vector<std::vector<float>> &plane)
+{
+    int wallCount = 0;
+    for (int nx = x - 1; nx <= x + 1; nx++) {
+	for (int ny = y - 1; ny <= y + 1; ny++) {
+	    if (nx >= 0 && nx < endX && ny >= 0 && ny < endY) {
+		if (nx != x || ny != y) {
+		    wallCount += plane[nx][ny];
+		}
+	    }
+	    else {
+		wallCount++;
+	    }
+	}
+    }
+    return wallCount;
 }
 
 int main()
@@ -133,24 +87,36 @@ int main()
     const int endX = 2 + screenWidth / CELL;
     const int endY = 2 + screenHeight / CELL;
 
-    srand(std::time(0));
-
     SetConfigFlags(FLAG_VSYNC_HINT);
     InitWindow(screenWidth, screenHeight, "Marching Squares");
 
     std::vector<std::vector<float>> plane(endX);
+    int randPerc = 50;
 
-    float xoff = 0.0;
-    float yoff = 0.0;
     for (int i{0}; i < endX; i++)
     {
-        xoff += 0.1;
         plane[i] = std::vector<float>(endY);
         for (int j{0}; j < endY; j++)
         {
-            if (i != endX - 1 && j != endY - 1)
-                plane[i][j] = perlinNoise(xoff, yoff, 6, 1.0 / 4.0, 2.0);
-            yoff += 0.1;
+            if (i != endX - 2 && j != endY - 2 && i != 0 && j != 0)
+                plane[i][j] = GetRandomValue(0, 100) < randPerc ? 1 : 0;
+            else
+                plane[i][j] = 1;
+        }
+    }
+
+    for (int _k{0}; _k < 8; _k++)
+    {
+        for (int i{0}; i < endX; i++)
+        {
+            for (int j{0}; j < endY; j++)
+            {
+                int neighbours = getNeighbours(i, j, endX, endY, plane);
+                if (neighbours > GetRandomValue(4, 5))
+                    plane[i][j] = 1;
+                else if (neighbours < 4)
+                    plane[i][j] = 0;
+            }
         }
     }
 
@@ -158,13 +124,15 @@ int main()
     {
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
         for (int i{0}; i < endX; i++)
         {
             for (int j{0}; j < endY; j++)
             {
                 if (i != endX - 1 && j != endY - 1)
                     drawContour(plane, i, j);
+                    if (plane[i][j] == 1)
+                        DrawRectangle(i * CELL, j * CELL, 2.0, 2.0, RAYWHITE);
             }
         }
 
